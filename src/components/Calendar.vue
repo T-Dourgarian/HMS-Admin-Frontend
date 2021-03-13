87424782859c5576d16ad71d9604ca1c07a5f6bb
T-Dourgarian <thomasdourgarian@gmail.com>
2021-03-13 09:59:12 -0600

    calendar colors and details

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,158 +1,204 @@
 <template>
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar
-          flat
-        >
-          <v-btn
-            outlined
-            class="mr-4"
-            color="grey darken-2"
-            @click="setToday"
-          >
-            Today
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="prev"
-          >
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="next"
-          >
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu
-            bottom
-            right
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                outlined
-                color="grey darken-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
-          
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          offset-x
-        >
-          <v-card
-            color="grey lighten-4"
-            min-width="350px"
-            flat
-          >
-            <v-toolbar
-              :color="selectedEvent.color"
-              dark
-            >
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-				<v-toolbar-title>
-					{{ selectedEvent.details }} - {{ selectedEvent.customerName }}
+	<v-row>
+		<v-col class='col-3'>
+			<BookingUtil />
+		</v-col>
+		<!-- HERERERERERER -->
+		<v-col>
+			<v-row class="fill-height">
+			<v-col>
+			<v-sheet height="64">
+				<v-toolbar
+				flat
+				>
+				<v-btn
+					outlined
+					class="mr-4"
+					color="grey darken-2"
+					@click="setToday"
+				>
+					Today
+				</v-btn>
+				<v-btn
+					fab
+					text
+					small
+					color="grey darken-2"
+					@click="prev"
+				>
+					<v-icon small>
+					mdi-chevron-left
+					</v-icon>
+				</v-btn>
+				<v-btn
+					fab
+					text
+					small
+					color="grey darken-2"
+					@click="next"
+				>
+					<v-icon small>
+					mdi-chevron-right
+					</v-icon>
+				</v-btn>
+				<v-toolbar-title v-if="$refs.calendar">
+					{{ $refs.calendar.title }}
 				</v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
+				<v-spacer></v-spacer>
+				<v-menu
+					bottom
+					right
+				>
+					<template v-slot:activator="{ on, attrs }">
+					<v-btn
+						outlined
+						color="grey darken-2"
+						v-bind="attrs"
+						v-on="on"
+					>
+						<span>{{ typeToLabel[type] }}</span>
+						<v-icon right>
+						mdi-menu-down
+						</v-icon>
+					</v-btn>
+					</template>
+					<v-list>
+					<v-list-item @click="type = 'day'">
+						<v-list-item-title>Day</v-list-item-title>
+					</v-list-item>
+					<v-list-item @click="type = 'week'">
+						<v-list-item-title>Week</v-list-item-title>
+					</v-list-item>
+					<v-list-item @click="type = 'month'">
+						<v-list-item-title>Month</v-list-item-title>
+					</v-list-item>
+					<v-list-item @click="type = '4day'">
+						<v-list-item-title>4 days</v-list-item-title>
+					</v-list-item>
+					</v-list>
+				</v-menu>
+				</v-toolbar>
+			</v-sheet>
+			<v-sheet height="600">
+				<v-calendar
+				ref="calendar"
+				v-model="focus"
+				color="primary"
+				:events="events"
+				:event-color="getEventColor"
+				:type="type"
+				@click:event="showEvent"
+				@click:more="viewDay"
+				@click:date="viewDay"
+				
+				></v-calendar>
+				<v-menu
+				v-model="selectedOpen"
+				:close-on-content-click="false"
+				:activator="selectedElement"
+				offset-x
+				>
+				<v-card
+					color="grey lighten-4"
+					min-width="350px"
+					flat
+				>
+					<v-toolbar
+					:color="selectedEvent.color"
+					dark
+					>
+					<v-btn icon>
+						<v-icon>mdi-pencil</v-icon>
+					</v-btn>
+					<v-toolbar-title v-if="selectedEvent.checkIn">
+						<v-row align="center" justify="center">
+							<v-col>
+								{{ selectedEvent.details }} - {{ selectedEvent.customerName }}
+							</v-col>
+							<v-col  style="fontSize: 13px;">
+								Check In:
+									<span style="margin: auto 0 auto 5px;">{{ selectedEvent.checkIn.getMonth() + 1 }}/{{ selectedEvent.checkIn.getDate()}}</span>
+							</v-col>
+							<v-col style="fontSize: 13px;">
+								Check Out:
+								<span style="margin: auto 0 auto 5px;">{{ selectedEvent.checkOut.getMonth() + 1 }}/{{ selectedEvent.checkOut.getDate() }}</span>
+							</v-col>
+						</v-row>
+					</v-toolbar-title>
+					</v-toolbar>
+					<v-card-text>
+						<v-row>
+							<v-col>
+								<v-row >
+									<v-col>
+										<div style="font-size:15px"><b>Details</b></div>
+										<div>
+											<v-icon small>fas fa-hotel</v-icon> 
+											<span style="margin: 0 0 0 5px;">Room - </span>
+											<span style="margin: auto 0 auto 5px;"> {{ selectedEvent.roomName }}</span>
+										</div>
 
-				<v-col>
-					<div>
-						<div style="font-size:15px"><b>Details</b></div>
+										<div>
+											<v-icon small>fas fa-users</v-icon>
+											<span style="margin: 0 0 0 5px;"> Guests - </span>
+											<span>{{ selectedEvent.numberOfGuests }}</span>
+										</div>
 
-						<div>
-							<v-icon x-small>fas fa-hotel</v-icon>
-							<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerName }}</span>
-						</div>
-					</div>
-				</v-col>
-				<v-col>
-					<div>
-						<v-icon x-small>fas fa-user</v-icon>
-						<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerName }}</span>
-					</div>
-					<div>
-						<v-icon x-small>fas fa-envelope</v-icon>
-						<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerEmail }}</span>
-					</div>
-					<div>
-						<v-icon x-small>fas fa-phone</v-icon>
-						<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerPhoneNumber }}</span>
-					</div>
-				</v-col>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                text
-                color="secondary"
-                @click="selectedOpen = false"
-              >
-                Cancel
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
+										<div>
+											<v-icon small>fas fa-moon</v-icon>
+											<span style="margin: 0 0 0 5px;">Nights - </span>
+											<span >{{ selectedEvent.numberOfNights }}</span>
+										</div>
+									</v-col>
+								</v-row>
+								<v-row>
+									<v-col>
+										<div style="font-size:15px"><b>Add-ons</b></div>
+										<div v-for="addOn in selectedEvent.addOns" :key="addOn.uuid">
+											<span style="margin: auto 0 auto 5px;"> - {{ addOn.name }}</span>
+										</div>
+									</v-col>
+								</v-row>
+							</v-col>
+						</v-row>
+						<v-row>
+							<v-col>
+								<div>
+									<v-icon x-small>fas fa-user</v-icon>
+									<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerName }}</span>
+								</div>
+								<div>
+									<v-icon x-small>fas fa-envelope</v-icon>
+									<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerEmail }}</span>
+								</div>
+								<div>
+									<v-icon x-small>fas fa-phone</v-icon>
+									<span style="margin: auto 0 auto 5px;"> - {{ selectedEvent.customerPhoneNumber }}</span>
+								</div>
+							</v-col>
+						</v-row>
+					</v-card-text>
+					<v-card-actions>
+					<v-btn
+						text
+						color="secondary"
+						@click="selectedOpen = false"
+					>
+						Cancel
+					</v-btn>
+					</v-card-actions>
+				</v-card>
+				</v-menu>
+			</v-sheet>
+			</v-col>
+		</v-row>
+		</v-col>
+	</v-row>
 </template>
 
 <script>
 import axios from 'axios';
+import BookingUtil from './BookingUtil'
 export default {
     data: () => ({
 		today: new Date().toISOString().substring(0,10),
@@ -176,6 +222,7 @@ export default {
 		dialog: false,
 		colors: 'blue'
     }),
+	components : { BookingUtil },
     created () {
       this.getBookings();
     },
@@ -185,6 +232,7 @@ export default {
 				const { data } = await axios.get('http://localhost:3000/api/booking');
 
 				console.log(data.bookings)
+				
 
 				this.events = data.bookings.map(booking => {
 					return {
@@ -192,10 +240,16 @@ export default {
 						details: 'Booking',
 						start: booking.checkIn.substring(0,10),
 						end: booking.checkOut.substring(0,10),
-						color:'red',
+						checkIn: new Date(booking.checkIn),
+						checkOut: new Date(booking.checkOut),
+						color: booking.color,
 						customerName: booking.customerDetails.name ? booking.customerDetails.name : 'N/A',
 						customerPhoneNumber: booking.customerDetails.phoneNumber  ? booking.customerDetails.phoneNumber : 'N/A',
-						customerEmail: booking.customerDetails.email ? booking.customerDetails.email : 'N/A'
+						customerEmail: booking.customerDetails.email ? booking.customerDetails.email : 'N/A',
+						roomName: booking.room[0].name,
+						addOns: booking.room[0].addOns,
+						numberOfGuests: booking.numberOfGuests,
+						numberOfNights: booking.numberOfNights
 					}
 				})
 

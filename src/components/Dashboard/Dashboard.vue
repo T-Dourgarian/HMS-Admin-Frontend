8a108bf2f55d530da4fa8d9f262c0ec04809c615
T-Dourgarian <thomasdourgarian@gmail.com>
2021-04-26 17:13:44 -0500

    check in and out modal

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,11 +1,9 @@
 <template>
-	<v-row>
+	<v-row class="ma-0" style="height: 92vh; maxHeight: 92vh; margin: 0 !important;">
 		<v-col cols="12">
-			Today's bookings 
-		</v-col>
-		<v-col cols="12">
-			<section>
 
+			<b>Today's Bookings</b>
+			<section>
 				<b-field label="Filter">
 					<b-select
 						v-model="filterOption"
@@ -41,37 +39,43 @@
 						{{ props.row.roomType[0].name }}
 					</b-table-column>
 
-					<b-table-column  label="Room Number" width="40" v-slot="props">
+					<b-table-column  label="Room No." width="40" v-slot="props">
 						{{ props.row.room[0].roomNumber }}
 					</b-table-column>
 
+					<b-table-column  label="Checked In" width="40" v-slot="props">
+						{{ props.row.checkedInDate ? props.row.checkedInDate : 'N/A'}}
+					</b-table-column>
+
+
+					<b-table-column  label="Checked Out" width="40" v-slot="props">
+						{{ props.row.checkedOutDate ? props.row.checkedOutDate : 'N/A'}}
+					</b-table-column>
+
 					<b-table-column  label="Action" width="40" v-slot="props">
-						<div v-if="isToday(props.row.checkIn)">
-							checkin
+						<div v-if="!props.row.checkedIn">
+							<b-button
+								@click="openCheckInOutDialog(props.row)"
+							>
+								Check In
+							</b-button>
 						</div>
 
-						<div v-else-if="isToday(props.row.checkOut)">
-							checkOut
+						<div v-else-if="!props.row.checkedOut">
+							<b-button 
+								@click="openCheckInOutDialog(props.row)"
+							>
+								Check Out
+							</b-button>
 						</div>
 
 						<div v-else>
-							asdf
+							Checked Out
 						</div>
+
 					</b-table-column>
 
-					<!-- <b-table-column field="user.first_name" label="First Name" sortable v-slot="props">
-						{{ props.row.user.first_name }}
-					</b-table-column>
-
-					<b-table-column field="user.last_name" label="Last Name" sortable v-slot="props">
-						{{ props.row.user.last_name }}
-					</b-table-column>
-
-					<b-table-column field="date" label="Date" sortable centered v-slot="props">
-						<span class="tag is-success">
-							{{ new Date(props.row.date).toLocaleDateString() }}
-						</span>
-					</b-table-column>
+<!-- 					
 
 					<b-table-column label="Gender" v-slot="props">
 						<span>
@@ -83,6 +87,51 @@
 					</b-table-column> -->
 
 				</b-table>
+
+				<div class="modal" v-if="checkInOutDialog && checkInOutBooking" :class="{'is-active': checkInOutDialog }">
+					<div class="modal-background">				
+					</div>
+					
+					<div class="modal-card">
+						<header class="modal-card-head">
+							<p class="modal-card-title"> Confirm Check {{ checkInOutBooking.checkedIn ? 'Out' : 'In' }} </p>
+							<button class="delete" aria-label="close" @click="checkInOutDialog = false"></button>
+						</header>
+						<section class="modal-card-body">
+
+							<h1>Confirm Details</h1>
+
+							<div>
+								{{ checkInOutBooking.customerDetails.name }}
+							</div>
+
+							<div>
+								{{ checkInOutBooking.customerDetails.email }}
+							</div>
+							
+							<div>
+								{{ checkInOutBooking.customerDetails.phoneNumber }}
+							</div>
+							
+						</section>
+
+						<footer class="modal-card-foot">
+							<b-button 
+								type="is-success"
+								@click="checkInOut(checkInOutBooking.uuid)"
+							>
+								Check {{ checkInOutBooking.checkedIn ? 'Out' : 'In' }}
+							</b-button>
+							<b-button
+								type="is-danger"
+								outlined
+								@click="checkInOutDialog = false"
+							>
+								Cancel
+							</b-button>
+						</footer>
+					</div>
+				</div>
 			</section>
 		</v-col>
 	</v-row>
@@ -91,25 +140,50 @@
 <script>
 import axios from 'axios';
 import moment from 'moment';
+
 export default {
 	data() {
 		return {
 			bookings: [],
 			filterOptions: ['all', 'check in', 'check out'],
-			filterOption: 'all'
+			filterOption: 'all',
+			checkInOutDialog: false,
+			checkInOutBooking: null
 		}
 	},
 	methods: {
 		async getBookings() {
-			const { data } = await axios.get(`http://localhost:3000/api/booking/${this.$store.state.user.company.uuid}`, {
-				params: {
-					today: true
-				}
-			});
+			try {
+				const { data } = await axios.get(`http://localhost:3000/api/booking/${this.$store.state.user.company.uuid}`, {
+					params: {
+						today: true
+					}
+				});
 
-			this.bookings = data.bookings;
+				this.bookings = data.bookings;
 
-			console.log('data', data.bookings);
+				console.log('data', data.bookings);
+			} catch(error) {
+				console.log(error)
+			}
+		},
+		async checkInOut(uuid) {
+
+			try {
+				await axios.put(`http://localhost:3000/api/booking/checkinout/${uuid}`, {
+					date: moment().format('YYYY-MM-DD HH:mm a')
+				});
+
+
+				this.getBookings();
+
+			} catch(error) {
+				error
+			}
+		},
+		openCheckInOutDialog(booking) {
+			this.checkInOutBooking = booking;
+			this.checkInOutDialog = true;
 		},
 		isToday(d1) {
 
